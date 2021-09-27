b5a2d800bf65f18e28e4a3b9775fe7744d4e8fac
sabertruth <48374071+sabertruth@users.noreply.github.com>
2021-09-27 13:22:56 +1300

    Unit test implemented
    Succesful

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  render(<App />);
+  //expect(linkElement).toBeInTheDocument();
+});
